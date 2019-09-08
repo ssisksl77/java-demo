02af1be7b0f26d2daaf2ecec7c774b8675321349
tom <ssisksl77@gmail.com>
2019-09-08 17:30:26 +0900

    백준 알고리즘 괄호 문제 풀이 완료

--- src/main/java/algorithm/bakjun/basic1/ds1/paren/Main.java
+++ src/main/java/algorithm/bakjun/basic1/ds1/paren/Main.java
@@ -1,5 +1,55 @@
 package algorithm.bakjun.basic1.ds1.paren;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
 
+/**
+ * 
+ * @author tom
+ * https://www.acmicpc.net/problem/9012
+ */
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int cnt = Integer.valueOf(br.readLine());
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < cnt; i++) {
+			String str = br.readLine();
+			sb.append((isParen(str) ? "YES" : "NO" ) + "\n");
+			
+		}
+		bw.write(sb.toString());
+		
+		bw.close();
+		br.close();
+		
+	}
+	
+	static boolean  isParen(String parens) {
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < parens.length(); i++) {
+			switch(parens.charAt(i)) {
+			case '(' :
+				stack.push('(');
+				break;
+			case ')' :
+				if(stack.isEmpty()) {
+					return false;
+				} else {
+					stack.pop();
+				}
+				break;
+			}
+		}
+		
+		if (!stack.isEmpty()) return false;
+		
+		return true;
+	}
 }
---
+++ src/main/java/algorithm/bakjun/basic1/ds1/stacksequence/Main.java
@@ -0,0 +1,12 @@
+package algorithm.bakjun.basic1.ds1.stacksequence;
+
+/**
+ * https://www.acmicpc.net/problem/1874
+ * @author tom
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
