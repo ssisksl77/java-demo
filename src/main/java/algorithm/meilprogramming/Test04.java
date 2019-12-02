2c0c02644c45d2a6b08b409760a1c0d99b4b4ea6
남영환 <ssisksl77@itembay.com>
2019-12-02 17:44:08 +0900

    매일 프로그래밍 수정

--- src/main/java/algorithm/meilprogramming/Test04.java
+++ src/main/java/algorithm/meilprogramming/Test04.java
@@ -1,4 +1,8 @@
 package algorithm.meilprogramming;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 정수(int)가 주어지면, 팰린드롬(palindrome)인지 알아내시오. 
  * 팰린드롬이란, 앞에서부터 읽으나 뒤에서부터 읽으나 같은 단어를 말합니다. 단, 정수를 문자열로 바꾸면 안됩니다.
@@ -42,5 +46,30 @@ public class Test04 {
 			}
 		}
 		System.out.println(res);
+	}
+	
+	private static void solve1(int n) {
+		if (n < 0) {
+			System.out.println(false);
+			return;
+		}
+		
+		if (n == 0) {
+			System.out.println(true);
+		}
+		
+		List<Integer> list = new ArrayList<>();
+		for(int r = 0; n > 0;) {
+			r = n % 10;
+			list.add(r);
+			n = n / 10;
+		}
+		for (int i = 0; i < list.size() / 2; i++) {
+			if (list.get(i) != list.get(list.size()-1-i)) {
+				System.out.println(false);
+				return;
+			}
+		}
+		System.out.println(true);
 	}
 }
